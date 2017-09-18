322cf77435ebcdaad4e68e19151b6a532baec589
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-18 18:54:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,8 @@ function increaseRankBy(n) {
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
 }
 }
-function deepestChild() {
-  var child = document.querySelector('#grand-node');
-  var deepestNode = child.children[0];
-  for (var i = 0; i < child.length; i++) {
-    deepestNode = deepestNode.children[0]
-}
-return deepestNode
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
+
 }
