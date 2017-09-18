11a1ef33c3bfc79239645b19b6cf401b17488d91
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-18 18:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget(){
 }
 function increaseRankBy(n) {
   var ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  for (let i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = (i = 1).toString()
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
 }
 }
